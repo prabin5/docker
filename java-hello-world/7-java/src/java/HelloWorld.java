23ff31d2b4a1b157c4ca663e2de729c2bf5b66d1
waymilky <rockzone2012@gmail.com>
2015-07-09 20:04:08 -0700

    Message Update

--- java-hello-world/7-java/src/java/HelloWorld.java
+++ java-hello-world/7-java/src/java/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello World Folks!");
+		System.out.println("Hello World Folks!\nThis is java test in docker image");
 	}
 }
